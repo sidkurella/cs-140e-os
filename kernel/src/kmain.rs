de3fc0b67d4bf2bc7af537a849fa5384db3778fb
Siddharth Kurella <siddharthkurella@gmail.com>
2019-02-15 00:14:03 -0800

    Add GPIO device driver

--- kernel/src/kmain.rs
+++ kernel/src/kmain.rs
@@ -19,5 +19,12 @@ pub mod shell;
 
 #[no_mangle]
 pub extern "C" fn kmain() {
-    // FIXME: Start the shell.
+    let mut pin = pi::gpio::Gpio::new(16).into_output();
+
+    loop {
+        pin.set();
+        pi::timer::spin_sleep_ms(250);
+        pin.clear();
+        pi::timer::spin_sleep_ms(250);
+    }
 }
