bef0469ae6a7746eac5eb4c41086f1e4c7a25b2b
Henry Sun <hsun324@gmail.com>
2019-02-20 15:27:42 -0800

    Fix UART and start Console impl

--- kernel/src/kmain.rs
+++ kernel/src/kmain.rs
@@ -33,15 +33,13 @@ pub static ALLOCATOR: Allocator = Allocator::uninitialized();
 
 pub static FILE_SYSTEM: FileSystem = FileSystem::uninitialized();
 
+use console::{kprint, kprintln, CONSOLE};
+
 #[no_mangle]
 #[cfg(not(test))]
 pub extern "C" fn kmain() {
-    let mut pin = pi::gpio::Gpio::new(16).into_output();
 
     loop {
-        pin.set();
-        pi::timer::spin_sleep_ms(250);
-        pin.clear();
-        pi::timer::spin_sleep_ms(250);
+        kprint!("a");
     }
 }
