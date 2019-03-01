8e11f50d9631092a28b9c360d8e54c4a8d7d13fd
Henry Sun <hsun324@gmail.com>
2019-02-28 16:58:12 -0800

    Add bouncepad for bootloader

--- bootloader/src/lang_items.rs
+++ bootloader/src/lang_items.rs
@@ -1,6 +1,8 @@
 #[lang = "eh_personality"] pub extern fn eh_personality() {}
 
-#[lang = "panic_fmt"] #[no_mangle] pub extern fn panic_fmt() -> ! { loop{} }
+use std::panic::PanicInfo;
+
+#[panic_handler] #[no_mangle] pub extern fn panic_fmt(info: &PanicInfo) -> ! { loop{} }
 
 #[no_mangle]
 pub unsafe extern fn memcpy(dest: *mut u8, src: *const u8, n: usize) -> *mut u8 {
