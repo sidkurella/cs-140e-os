cd85b3cee11bec260750498aa32f90d7af6a19fe
Siddharth Kurella <siddharthkurella@gmail.com>
2020-02-19 15:47:02 -0800

    Implement Rust bindings for SD driver

--- kernel/src/fs/sd.rs
+++ kernel/src/fs/sd.rs
@@ -1,5 +1,6 @@
 use std::io;
 use fat32::traits::BlockDevice;
+use pi::timer;
 
 extern "C" {
     /// A global representing the last SD controller error that occured.
@@ -25,12 +26,42 @@ extern "C" {
     fn sd_readsector(n: i32, buffer: *mut u8) -> i32;
 }
 
-// FIXME: Define a `#[no_mangle]` `wait_micros` function for use by `libsd`.
-// The `wait_micros` C signature is: `void wait_micros(unsigned int);`
+#[no_mangle]
+fn wait_micros(us: u32) {
+    timer::spin_sleep_us(us as u64);
+}
 
 #[derive(Debug)]
 pub enum Error {
-    // FIXME: Fill me in.
+    TimedOut,
+    CommandFailure,
+    Other(i64)
+}
+
+impl From<i64> for Error {
+    fn from(e: i64) -> Error {
+        match e {
+            -1 => Error::TimedOut,
+            -2 => Error::CommandFailure,
+             c => Error::Other(c)
+        }
+    }
+}
+
+impl From<Error> for io::Error {
+    fn from(e: Error) -> io::Error {
+        match e {
+            Error::TimedOut => io::Error::new(
+                io::ErrorKind::TimedOut, "SD timeout"
+            ),
+            Error::CommandFailure => io::Error::new(
+                io::ErrorKind::Other, "SD command send failure"
+            ),
+            Error::Other(code) => io::Error::new(
+                io::ErrorKind::Other, "SD miscellaneous error"
+            ),
+        }
+    }
 }
 
 /// A handle to an SD card controller.
@@ -40,7 +71,12 @@ pub struct Sd;
 impl Sd {
     /// Initializes the SD card controller and returns a handle to it.
     pub fn new() -> Result<Sd, Error> {
-        unimplemented!("Sd::new()")
+        let code = unsafe { sd_init() };
+        if code == 0 {
+            Ok(Sd)
+        } else {
+            Err(From::from(code as i64))
+        }
     }
 }
 
@@ -58,7 +94,20 @@ impl BlockDevice for Sd {
     ///
     /// An error of kind `Other` is returned for all other errors.
     fn read_sector(&mut self, n: u64, buf: &mut [u8]) -> io::Result<usize> {
-        unimplemented!("Sd::read_sector()")
+        if buf.len() < 512 {
+            Err(io::Error::new(
+                io::ErrorKind::InvalidInput, "SD read: buffer is too short"
+            ))
+        } else if n > (1 << 31) - 1 {
+            Err(io::Error::new(
+                io::ErrorKind::InvalidInput, "read is too large"
+            ))
+        } else {
+            match unsafe { sd_readsector(n as i32, buf.as_mut_ptr()) } {
+                0 => Err(From::<Error>::from(From::from(unsafe { sd_err }))),
+                bytes => Ok(bytes as usize)
+            }
+        }
     }
 
     fn write_sector(&mut self, _n: u64, _buf: &[u8]) -> io::Result<usize> {
